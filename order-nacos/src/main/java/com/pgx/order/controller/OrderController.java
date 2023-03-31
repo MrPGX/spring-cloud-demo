c7fb73708385763225977494ce1511be52dd8d8b
panguoxing <kkak798@aliyun.com>
2023-03-31 09:38:41 +0800

    company commit

---
+++ order-nacos/src/main/java/com/pgx/order/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.pgx.order.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/order/*")
+public class OrderController {
+    @Qualifier("restTemplate")
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping("add")
+    public String add(){
+        System.out.println("下单成功！");
+        ResponseEntity<String> msg = restTemplate.getForEntity("http://stock-service/stock/reduct",String.class);
+
+        return "hello world " + msg;
+    }
+}

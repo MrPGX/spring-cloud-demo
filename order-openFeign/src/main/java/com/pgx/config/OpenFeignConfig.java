f858050259165e6b03002298504cb3e8f24d6ecd
panguoxing <kkak798@aliyun.com>
2023-07-31 17:10:10 +0800

    openFeign

--- order-openFeign/src/main/java/com/pgx/config/OpenFeignConfig.java
+++ order-openFeign/src/main/java/com/pgx/config/OpenFeignConfig.java
@@ -1,13 +1,20 @@
 package com.pgx.config;
 
 import feign.Logger;
+import feign.Request;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenFeignConfig {
+
     @Bean
     public Logger.Level feignLoggerLevl(){
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Request.Options options(){
+        return new Request.Options(5_000,100_000);
     }
 }
